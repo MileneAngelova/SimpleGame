fc0d5740ea85d64c4c1dc6b673e62f5808f4cd1a
M.Angelova <104679767+MileneAngelova@users.noreply.github.com>
2023-05-08 00:37:21 +0300

    Error catched

--- simpleGame.js
+++ simpleGame.js
@@ -1,43 +1,64 @@
-function computerPlay() {
+function computerChoice() {
     const possibleChoices = ['rock', 'scissors', 'paper'];
     return possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
 }
 
+let roundCounter = 1;
+
+function playerChoice() {
+    let playerDecision = prompt(`Round ${roundCounter}\nMake your choice! Type Rock, Paper or Scissors.`);
+    try {
+        return playerDecision.trim().toLowerCase();
+    } catch (error) {
+        console.log('Sorry to see you leave!');
+    }
+}
+
 let gamesWonByPlayer = 0;
 let gamesWonByComputer = 0;
+
 function playSingleRound(playerSelection, computerSelection) {
     let roundResultMessage = '';
     let round = 1;
 
     while (round > 0) {
-        playerSelection = prompt('Make your choice! Type Rock, Paper or Scissors.').toLowerCase().trim();
-        computerSelection = computerPlay();
+        computerSelection = computerChoice();
+        playerSelection = playerChoice();
 
-        if (playerSelection === computerSelection) {
+        if (playerSelection === null || playerSelection === undefined) {
+            break;
+        } else if (playerSelection === computerSelection) {
             roundResultMessage = 'Game-draw! Play again: Type Rock, Paper or Scissors.';
+            roundCounter++;
         } else if (playerSelection === 'rock') {
             if (computerSelection === 'paper') {
                 roundResultMessage = 'You Lose the Round! Paper beats Rock';
                 gamesWonByComputer++;
+                roundCounter++;
             } else {
                 roundResultMessage = `You won the Round! ${playerSelection} beats ${computerSelection}!`;
                 gamesWonByPlayer++;
+                roundCounter++;
             }
         } else if (playerSelection === 'paper') {
             if (computerSelection === 'scissors') {
                 roundResultMessage = 'You Lose the Round! Scissors beats Paper';
                 gamesWonByComputer++;
+                roundCounter++;
             } else {
                 roundResultMessage = `You won the Round! ${playerSelection} beats ${computerSelection}!`;
                 gamesWonByPlayer++;
+                roundCounter++;
             }
         } else if (playerSelection === "scissors") {
             if (computerSelection === "rock") {
                 roundResultMessage = 'You Lose the Round! Rock beats Scissors';
-                gamesWonByComputer++
+                gamesWonByComputer++;
+                roundCounter++;
             } else {
                 roundResultMessage = `You won the Round! ${playerSelection} beats ${computerSelection}!`;
                 gamesWonByPlayer++;
+                roundCounter++;
             }
         } else {
             console.log('Invalid choice.Make another choice...');
@@ -49,11 +70,9 @@ function playSingleRound(playerSelection, computerSelection) {
     return roundResultMessage;
 }
 
-function playFiveRoundsGame() {
-    for (let i = 1; i <= 5; i++) {
-        console.log(playSingleRound());
-    }
-
+function printResult() {
+    console.log('Your score: ' + gamesWonByPlayer);
+    console.log('Computer score: ' + gamesWonByComputer);
     if (gamesWonByPlayer > gamesWonByComputer) {
         console.log('Congratulations! You won the game!');
     } else if (gamesWonByPlayer === gamesWonByComputer) {
@@ -61,6 +80,18 @@ function playFiveRoundsGame() {
     } else {
         console.log('Sorry you lost! Computer won the game.');
     }
+}
+
+function playFiveRoundsGame() {
+    alert(`Welcome!\nThis game has five rounds. Click \"OK"\ to continue`);
+
+    for (let i = 1; i <= 5; i++) {
+        // if (playSingleRound() === '') {
+        //     break;
+        // }
+        console.log(playSingleRound());
+    }
+    printResult();
 }
 
 playFiveRoundsGame();
